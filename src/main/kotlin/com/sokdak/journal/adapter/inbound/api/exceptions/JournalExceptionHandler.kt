7f2272b4536b5c0afcf934281e1d0f688cea17ec
Jieun Park <140596167+jieunpark626@users.noreply.github.com>
2025-12-31 14:07:10 +0900

    refactor(journal): rename exception handler

--- src/main/kotlin/com/sokdak/journal/adapter/inbound/api/exceptions/JournalExceptionHandler.kt
+++ src/main/kotlin/com/sokdak/journal/adapter/inbound/api/exceptions/JournalExceptionHandler.kt
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
 @RestControllerAdvice
-class GlobalExceptionHandler {
+class JournalExceptionHandler {
 
     // 404 - 일기 없음
     @ExceptionHandler(JournalNotFoundException::class)
